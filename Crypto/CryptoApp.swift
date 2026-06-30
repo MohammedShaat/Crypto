4340799f1ca5f9f8e80eede1dd42a7db619e6a2a
Mohammed Shaat <mohammedshaat.it@gmail.com>
2026-06-30 02:29:25 -0400

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Mohammed on 6/30/26.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
